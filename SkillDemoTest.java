1649b6c44a31c143c756aaa2febaff8855c336e5
imsanika03 <imsanika03@gmail.com>
2022-05-01 23:17:25 -0700

    new

---
+++ SkillDemoTest.java
@@ -0,0 +1,18 @@
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*; 
+
+
+
+public class SkillDemoTest {
+
+    @Test
+    public void checkMultiply(){
+
+        assertEquals(0, SkillDemoCode.multiply(1, 1));
+    }
+    
+}
